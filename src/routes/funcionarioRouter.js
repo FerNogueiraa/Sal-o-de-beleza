b11f7ca35f396a7602161bd06119e5c2c785bf87
Fernando Nogueira <137718580+FerNogueiraa@users.noreply.github.com>
2024-09-10 15:30:38 -0300

    Removendo Arquivos Front

--- src/routes/funcionarioRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import funcionarioController from '../controllers/funcionarioController.js';
-
-const router = Router();
-
-router.post('/', funcionarioController.criarFuncionario);
-router.get('/',funcionarioController.listarFuncionarios);
-
-export default router;
