1f53821c88dee9a7a2144d46a103090b72200e09
Fernando Nogueira <137718580+FerNogueiraa@users.noreply.github.com>
2024-09-14 20:40:47 -0300

    Finalização Cadastro

--- interface/src/main.jsx
+++ interface/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Cadastro from './pages/Cadastro'
+import Cadastro from './pages/cadastro'
+import Login from './pages/login'
 import './index.css'
 
 
